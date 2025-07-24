f80cd07330a4326935828e85c2177be10c3d470d
Antimundo <pablo.rubio.ru@gmail.com>
2025-07-25 00:01:44 +0200

    Refactor and order various files

--- scenes/gameplay/game_finish.gd
+++ scenes/gameplay/game_finish.gd
@@ -4,19 +4,19 @@ signal game_finished
 
 @onready var player_character = $"../PlayerCharacter"
 
-var final_cinematic = preload("res://scenes/finial_cinematic/final_cinematic.tscn").instantiate()
+var final_cinematic = preload("res://scenes/final_cinematic/final_cinematic.tscn").instantiate()
 var is_game_finished := false
 
 func _process(_delta):
 	if is_game_finished:
 		return
 	if player_character.position.y < position.y - 100 and !is_game_finished:
-		$StaticBody2D.position.x = 300
+		is_game_finished = true
 		game_finished.emit()
 		$AudioStreamPlayer.play()
-		var tween = create_tween()
-		tween.tween_property($Sprite2D, "modulate:a", 1, .5)
+		var tween := create_tween()
+		$CanvasLayer.visible = true
+		tween.tween_property($CanvasLayer/ColorRect, "modulate:a", 1, .5)
 		await tween.finished
-		#await get_tree().create_timer(.5)
-		get_tree().change_scene_to_file("res://scenes/finial_cinematic/final_cinematic.tscn")
+		get_tree().change_scene_to_file("res://scenes/final_cinematic/final_cinematic.tscn")
 		queue_free()
